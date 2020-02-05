c1b52c3921e00b92199a4e27762ba9aef9daba14
Job Castrop <j.castrop@onetomarket.nl>
2020-02-05 13:07:53 +0100

    Create s1c5.py

---
+++ s1c5.py
@@ -0,0 +1,15 @@
+def repeating_xor_key(input_str, key):
+    output = b''
+    index = 0
+    for byte in input_str:
+        output += bytes([byte ^ key[index]])
+        index += 1
+        if(index >= len(key)):
+            index = 0
+    return output
+
+str1 = str.encode(input('String to encrypt: '))
+key = str.encode(input('Key to encrypt with: '))
+
+output = repeating_xor_key(str1, key)
+print(output.hex())
